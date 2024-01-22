68806616847e83f32b1c05641589a2e3d31ad2a7
yago1994 <yagoaral@hotmail.com>
2024-01-22 14:27:19 -0500

    Small language tweaks; increased questions font size;

--- choa-diabetes-education/Extensions/UIFont+Extensions.swift
+++ choa-diabetes-education/Extensions/UIFont+Extensions.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 extension UIFont {
-    public static let gothamRoundedBold16 = UIFont(name: "GothamRounded-Bold", size: 16.0)!
-    public static let avenirLight14 = UIFont(name: "Avenir-Light", size: 14.0)!
+    public static let gothamRoundedBold16 = UIFont(name: "GothamRounded-Bold", size: 18.0)!
+    public static let avenirLight14 = UIFont(name: "Avenir-Light", size: 16.0)!
     public static let avenir17 = UIFont(name: "Avenir", size: 17)
     public static let avenirMedium20 = UIFont(name: "Avenir-Medium", size: 20.0)!
     public static let avenir14 = UIFont(name: "Avenir", size: 14)
